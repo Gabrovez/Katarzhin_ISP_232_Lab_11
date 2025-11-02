216ee5e648274995ae1b0f02bbf6970567ed51c3
Gabrovez <gabrovez321@gmail.com>
2025-11-02 19:52:35 +0300

    Шаг 2. Разработка простого приложения

--- Form1.cs
+++ Form1.cs
@@ -16,5 +16,20 @@ namespace FirstProgram
         {
             InitializeComponent();
         }
+
+        private void Form1_Click(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Hello");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
     }
 }
